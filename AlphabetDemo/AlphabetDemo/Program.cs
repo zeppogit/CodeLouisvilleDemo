fb4427cb5b98ca5d73a1e5e56fbf018e96ca0bb0
Joe Sweeney <jsweeney@Joes-MacBook-Pro.local>
2022-01-31 20:40:57 -0500

    add AlphabetDemo project for Tues Feb 1 class

---
+++ AlphabetDemo/AlphabetDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace AlphabetDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintAlphabet(); PrintAlphabetBackwards(); PrintAlphabetSkipOneLetter();
+        }
+        static void PrintAlphabet()
+        {
+            string alphabet = string.Empty;
+            for (char alpha = 'A'; alpha <= 'Z'; alpha++)
+            {
+                alphabet += alpha;
+            }
+            Console.WriteLine(alphabet);
+        }
+        static void PrintAlphabetBackwards()
+        {
+            string alphabet = string.Empty;
+            for (char alpha = 'Z'; alpha >= 'A'; alpha--)
+            {
+                alphabet += alpha;
+            }
+            Console.WriteLine(alphabet);
+        }
+        static void PrintAlphabetSkipOneLetter()
+        {
+            string alphabet = string.Empty;
+            for (char alpha = 'A'; alpha <= 'Z'; alpha = (char)(alpha + 2))
+            {
+                alphabet += alpha;
+            }
+            Console.WriteLine(alphabet);
+        }
+    }
+}
+
