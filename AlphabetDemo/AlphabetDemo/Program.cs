1017cd97a281d21dc6b23d00c775ef1fe96c68bc
zeppogit <zeppocode@gmail.com>
2022-02-06 20:39:47 -0500

    to add user interaction to choose which method of printing the alphabet to use, and if skipping letters, the user enters the number of letters to skip.

--- AlphabetDemo/AlphabetDemo/Program.cs
+++ AlphabetDemo/AlphabetDemo/Program.cs
@@ -5,9 +5,39 @@ namespace AlphabetDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(PrintAlphabet());
-            Console.WriteLine(PrintAlphabetBackwards());
-            Console.WriteLine(PrintAlphabetSkipOneLetter(3));
+            Console.WriteLine("Welcome to the alphabet printer. \n If you would like to print the alphabet, enter 1.\n If you would like to print the alphabet backwards, enter 2. \n If you would like to only print every 'nth' letter of the alphabet, enter 3. \n\n");
+
+            //int choice = Console.ReadLine();
+
+            int choice = Int32.Parse(Console.ReadLine());
+            switch (choice)
+            {
+
+                case 1:
+
+                    Console.WriteLine(PrintAlphabet());
+                    break;
+
+
+                case 2: // statement sequence
+                    Console.WriteLine(PrintAlphabetBackwards());
+                    break;
+
+                case 3:
+                    Console.WriteLine("Enter a value for 'n' and this will now print every 'nth' letter of the alphabet. \n");
+                    int nth = Int32.Parse(Console.ReadLine());
+                    Console.WriteLine(PrintAlphabetSkipLetters(nth));
+                    break;
+
+                default:
+                    break;
+
+            }
+
+            //Console.ReadLine(int num);
+
+            
+            //
         }
 
         static string PrintAlphabet()
@@ -30,7 +60,7 @@ namespace AlphabetDemo
             return alphabet;
         }
 
-        static string PrintAlphabetSkipOneLetter(int numSkipped)
+        static string PrintAlphabetSkipLetters(int numSkipped)
         {
             string alphabet = string.Empty;
             for (char alpha = 'A'; alpha <= 'Z'; alpha = (char)(alpha + numSkipped))
