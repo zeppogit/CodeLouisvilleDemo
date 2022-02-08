14cf2e8050f95ff09fbc973583a6f6be4881b91a
zeppogit <zeppocode@gmail.com>
2022-02-08 13:59:39 -0500

    to refactor to declare variable named choice in the main and get rid of variable i

--- AlphabetDemo/AlphabetDemo/Program.cs
+++ AlphabetDemo/AlphabetDemo/Program.cs
@@ -7,20 +7,13 @@ namespace AlphabetDemo
         {
             Console.WriteLine("Welcome to the alphabet printer. \n\n ");
 
-            int i = 0;
+            int choice = 0;
             do
             {
                 Console.WriteLine("\n\nIf you would like to print the alphabet, enter 1.\nIf you would like to print the alphabet backwards, enter 2. \nIf you would like to only print every 'nth' letter of the alphabet, enter 3. \n\nEnter 4 to exit the program.");
 
-                int choice = Int32.Parse(Console.ReadLine());
+                choice = Int32.Parse(Console.ReadLine());
 
-                if (choice == 4)
-                {
-                    i = choice;
-                    Console.WriteLine("Goodbye!");
-                }
-                else
-                {
                     switch (choice)
                     {
                         case 1:
@@ -40,11 +33,14 @@ namespace AlphabetDemo
                             Console.WriteLine(PrintAlphabetSkipLetters(nth));
                             break;
 
-                        default:
+                    case 4:
+                            Console.WriteLine("Goodbye!");
+                            break;
+
+                    default:
                             break;
                     }
-                }
-            } while (i != 4);
+            } while (choice != 4);
         }
 
         static string PrintAlphabet()
