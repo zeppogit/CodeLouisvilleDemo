e753b2cf7ecedf8ebc1b70007c6316ddde3dd303
zeppogit <zeppocode@gmail.com>
2022-02-15 17:11:45 -0500

    to correct code typo in program.cs line 10

--- CLouPractice/Program.cs
+++ CLouPractice/Program.cs
@@ -7,7 +7,7 @@ namespace CodeLouPractice
     {
         static void Main(string[] args)
         {
-            bool doesThisMakeSense = CodeLouisvilleProjectBase.Prompt4YesNo("Does this make sense?");
+            bool doesThisMakeSense = CodeLouisvilleAppBase.Prompt4YesNo("Does this make sense?");
             if (doesThisMakeSense)
                 Console.WriteLine("I've got this!!!");
             else
